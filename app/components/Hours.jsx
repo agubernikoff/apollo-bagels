60ebdd82d42ea9622460f0901a458854c989fc83
alexander gubernikoff <agubernikoff@gmail.com>
2025-01-07 16:19:36 -0500

    hours color conditions + transitions

--- app/components/Hours.jsx
+++ app/components/Hours.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {useLocation} from '@remix-run/react';
 
 function Hours() {
+  const {pathname} = useLocation();
+  function style() {
+    return {
+      color: pathname !== '/' && pathname !== '/info' ? 'var(--blue)' : null,
+      borderColor:
+        pathname !== '/' && pathname !== '/info' ? 'var(--blue)' : null,
+    };
+  }
   return (
     <div className="hours-container">
-      <p>HOURS</p>
-      <p>
+      <p style={style()}>HOURS</p>
+      <p style={style()}>
         MONDAY: 7AM-5PM
         <br />
         TUESDAY: 7AM-5PM
