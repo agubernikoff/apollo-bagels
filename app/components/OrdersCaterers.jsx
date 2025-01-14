65d784d8696a55eaf8ffbe119dd0309faaaf0651
alexander gubernikoff <agubernikoff@gmail.com>
2025-01-13 23:15:24 -0500

    animate order/catering

--- app/components/OrdersCaterers.jsx
+++ app/components/OrdersCaterers.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation} from 'react-router-dom';
+import {motion} from 'framer-motion';
 
 function OrdersCaterers({data}) {
   const {pathname} = useLocation();
-  console.log(data);
   return (
     <div className="order-cater-container">
       <div className="order-cater-text-container">
@@ -33,64 +33,97 @@ function OrdersCaterers({data}) {
           .map((location) => {
             const {title, address, orderLink, cateringLink} = location;
 
-            const formattedAddress = (
-              <>
-                {address.street},{<br />}
-                {address.city}, {address.state} {address.postalCode}
-              </>
-            );
-            const link =
-              pathname === '/order'
-                ? orderLink
-                : pathname === '/catering'
-                ? cateringLink
-                : '#';
-
             return (
-              <div key={location._id} className="location-item">
-                <p
-                  style={{
-                    backgroundColor:
-                      pathname === '/order' ? 'var(--blue)' : 'var(--yellow)',
-                  }}
-                >
-                  <a
-                    href={link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="location-link"
-                    style={{
-                      color:
-                        pathname === '/order' ? 'var(--creme)' : 'var(--blue)',
-                    }}
-                  >
-                    {title}
-                  </a>
-                </p>
-                <p
-                  style={{
-                    backgroundColor:
-                      pathname === '/order' ? 'var(--blue)' : 'var(--yellow)',
-                    fontFamily: 'HAL',
-                  }}
-                >
-                  <a
-                    href={link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{
-                      color:
-                        pathname === '/order' ? 'var(--creme)' : 'var(--blue)',
-                    }}
-                  >
-                    {formattedAddress}
-                  </a>
-                </p>
-              </div>
+              <Location
+                title={title}
+                address={address}
+                orderLink={orderLink}
+                cateringLink={cateringLink}
+                pathname={pathname}
+                key={location._id}
+              />
             );
           })}
       </div>
     </div>
+  );
+}
+
+function Location({title, address, orderLink, cateringLink, pathname}) {
+  const [hovered, setHovered] = useState(false);
+  const formattedAddress = (
+    <>
+      {address.street},{<br />}
+      {address.city}, {address.state} {address.postalCode}
+    </>
+  );
+  const link =
+    pathname === '/order'
+      ? orderLink
+      : pathname === '/catering'
+      ? cateringLink
+      : '#';
+
+  return (
+    <motion.div
+      key={location._id}
+      className="location-item"
+      initial={{x: 0}}
+      animate={{x: hovered ? '1vw' : 0}}
+      transition={{ease: 'easeInOut'}}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <motion.p
+        initial={{
+          backgroundColor:
+            pathname === '/order' ? 'var(--blue)' : 'var(--yellow)',
+        }}
+        animate={{
+          backgroundColor: hovered
+            ? 'var(--green)'
+            : pathname === '/order'
+            ? 'var(--blue)'
+            : 'var(--yellow)',
+        }}
+      >
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="location-link"
+          style={{
+            color: pathname === '/order' ? 'var(--creme)' : 'var(--blue)',
+          }}
+        >
+          {title}
+        </a>
+      </motion.p>
+      <motion.p
+        initial={{
+          backgroundColor:
+            pathname === '/order' ? 'var(--blue)' : 'var(--yellow)',
+        }}
+        animate={{
+          backgroundColor: hovered
+            ? 'var(--green)'
+            : pathname === '/order'
+            ? 'var(--blue)'
+            : 'var(--yellow)',
+        }}
+      >
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            color: pathname === '/order' ? 'var(--creme)' : 'var(--blue)',
+          }}
+        >
+          {formattedAddress}
+        </a>
+      </motion.p>
+    </motion.div>
   );
 }
 
