4b981009ab8fcd917beeba0f8075ffa5b031fec5
alexander gubernikoff <agubernikoff@gmail.com>
2025-01-27 17:26:43 -0500

    key

--- app/sanity/SanityExternalLink.jsx
+++ app/sanity/SanityExternalLink.jsx
@@ -4,7 +4,7 @@ function SanityExternalLink({value, children}) {
   return (
     <a href={value.url} rel="noopener noreferrer" target="_blank">
       {children.map((child) => (
-        <span key={child.key}>{child}</span>
+        <span key={child.key || child}>{child}</span>
       ))}
     </a>
   );
