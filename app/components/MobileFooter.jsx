7c8d741acfe50c6f617d52f60c0275a8acc07ac7
alexander gubernikoff <agubernikoff@gmail.com>
2025-02-04 18:34:04 -0500

    various styling fixes for jumpy mobile browser bar

--- app/components/MobileFooter.jsx
+++ app/components/MobileFooter.jsx
@@ -291,7 +291,7 @@ export default function MobileFooter({hours}) {
       ref={ref}
       className="mobile-footer"
       style={{
-        marginBottom: `calc(100vh - ${
+        marginBottom: `calc(100dvh - ${
           ref?.current?.offsetHeight || 0
         }px - var(--header-height) )`,
         // transform: `translateY(-${footerY}dvh)`,
