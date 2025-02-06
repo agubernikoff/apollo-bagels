530d000ecb600369235d0230e6d63c52a55888cc
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-02-06 18:58:16 -0500

    header height

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -117,11 +117,11 @@ export function HeaderMenu({
       if (ref.current) {
         document.documentElement.style.setProperty(
           '--header-height',
-          `calc(${ref.current.offsetHeight}px + 3rem)`,
+          `calc(${ref.current.offsetHeight}px + 4rem)`,
         );
         document.documentElement.style.setProperty(
           '--mobile-header-height',
-          `calc(${ref.current.offsetHeight}px + 3rem)`,
+          `calc(${ref.current.offsetHeight}px + 4rem)`,
         );
       }
     };
